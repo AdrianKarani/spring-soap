4cd30b9d264af78eca0dee0504eb486755e64e3c
Adrian Karani <karanimuturi@gmail.com>
2019-10-17 12:11:20 +0300

    Initial commit

---
+++ src/test/java/soapb/soap/SoapApplicationTests.java
@@ -0,0 +1,13 @@
+package soapb.soap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
